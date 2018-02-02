b9cb55ca48f2b76ee011aa999adfb2b6df2bdecf
Allen <allen@16colorgames.com>
2018-02-02 14:11:10 -0600

    Added a few more comments, fixed some issues with windows launching

--- python/main.py
+++ python/main.py
@@ -10,9 +10,9 @@ import pickle
 
 
 # This section is for global variables
-theBot = Bot(description="Squid Nomic bot for Discord",
-             command_prefix="!", pm_help=False)
+theBot = Bot(description="Squid Nomic bot for Discord", command_prefix="!", pm_help=False)
 users = {}
+uptime = 0
 generation_period = 1
 save_period = 60
 update_period = 5
@@ -28,6 +28,7 @@ class User():
         self.data = {}
 
     def addPower(self):
+        global update_msg
         # how much space is left in the battery
         battery_room = self.getAttr("battery") - self.getAttr("watts")
         if battery_room > 0:
@@ -35,7 +36,6 @@ class User():
                 self.setAttr("watts", round(self.getAttr(
                     "watts") + self.getAttr("generation"), 2))
             else:
-                global update_msg
                 if update_msg is not None:
                     msg = self.mention + " has a full battery"
                     theBot.loop.create_task(sendMessage(update_msg.channel, msg))
@@ -46,7 +46,6 @@ class User():
             self.setAttr("overflow", round(self.getAttr(
                 "overflow") + self.getAttr("generation"), 2))
         if self.getAttr("overflow") >= watt_win:
-            global update_msg
             if update_msg is not None:
                 msg = "@" + self.mention + " has won"
                 theBot.loop.create_task(sendMessage(update_msg.channel, msg))
@@ -90,6 +89,7 @@ async def on_ready():
     print('Use this link to invite {}:'.format(theBot.user.name))
     print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(
                                                                                     theBot.user.id))
+
     with open("./data/users.pickle", 'rb') as in_s:  # open the file for reading
         while True:
             try:
@@ -97,6 +97,14 @@ async def on_ready():
                 users[o.name] = o  # load the object into the list
             except EOFError:  # We have finished the file
                 break  # so we can leave the loop
+
+    with open("./data/general.pickle", 'rb') as in_s:  # open the file for reading
+        try:
+            global uptime
+            uptime = pickle.load(in_s)
+        except EOFError:  # We have finished the file
+            print("Couldn't finish readinf file")
+
     theBot.loop.create_task(save_task())
     theBot.loop.create_task(genPower())
     theBot.loop.create_task(update_view())
@@ -104,25 +112,17 @@ async def on_ready():
 
 
 # This section is for custom commands.
-@theBot.command(pass_context=True)
-async def save(ctx):
-    msg = 'Manually saving user data'
-    await theBot.say(msg)
-    save()
-
-
-@theBot.command(pass_context=True)
+@theBot.command(pass_context=True, help='Displays basic game info.')
 async def info(ctx):
-    msg = 'Info: ' + str(ctx.message.channel.server)
+    msg = 'Info: \nUptime: ' + str(uptime)
     await theBot.say(msg)
 
 
-@theBot.command(pass_context=True)
+@theBot.command(pass_context=True, help='Attempts to add the sender to the user list')
 async def join(ctx):
     if not str(ctx.message.author) in users:  # if the user isn't already in the list
         users[str(ctx.message.author)] = User(str(ctx.message.author), ctx.message.author.mention)
-        msg = '{0.author.mention} has been added to the users list'.format(
-            ctx.message)
+        msg = '{0.author.mention} has been added to the users list'.format(ctx.message)
         await theBot.say(msg)
     else:
         msg = '{0.author.mention} is already in the users list'.format(
@@ -130,11 +130,11 @@ async def join(ctx):
         await theBot.say(msg)
 
 
-@theBot.command(pass_context=True)
+@theBot.command(pass_context=True, help='Attempts to remove the sender from the user list')
 async def leave(ctx):
     if str(ctx.message.author) in users:  # if the user is already in the list
         users.pop(str(ctx.message.author), None)
-        msg = '{0.author.mention} has been removed to the users list'.format(
+        msg = '{0.author.mention} has been removed from the users list'.format(
             ctx.message)
         await theBot.say(msg)
     else:
@@ -142,7 +142,14 @@ async def leave(ctx):
         await theBot.say(msg)
 
 
-@theBot.command(pass_context=True)
+@theBot.command(pass_context=True, help='Manually saves game data')
+async def save(ctx):
+    msg = 'Manually saving data'
+    await theBot.say(msg)
+    save()
+
+
+@theBot.command(pass_context=True, help='Request an auto-updating message')
 async def view(ctx):
     msg = ""
     for key, user in users.items():
@@ -154,24 +161,22 @@ async def view(ctx):
 
 
 # Put your other functions here
-async def update_view():
-    await theBot.wait_until_ready()
-    while True:  # set this up as an infinite loop
-        if update_msg is not None:  # if we have set a place to update the display
-            msg = ""
-            for key, user in users.items():
-                msg = msg + str(key) + ":\n--Battery: " + str(user.getAttr("watts")) + "/" + str(
-                    user.getAttr("battery")) + "W\n--Overflow: " + str(
-                    user.getAttr("overflow")) + "W\n\n"
-            await theBot.edit_message(update_msg, msg)  # send the edit request
-        await asyncio.sleep(update_period)  # wait a specified amount of time
+def get_user_or_None(member):
+    """Checks if a discord user has an associated nomic user"""
+    if str(member) in users:
+        return users[str(member)]
+    else:
+        return None
 
 
 async def genPower():
+    """Function to periodically generate power"""
+    global uptime
     await theBot.wait_until_ready()
     while True:
         for key, user in users.items():
             user.addPower()  # This adds the specified number of watts
+        uptime = uptime + 1
         await asyncio.sleep(generation_period)
 
 
@@ -184,14 +189,35 @@ async def save_task():
         await asyncio.sleep(save_period)
 
 
+def save():
+    with open("./data/users.pickle", 'wb') as out_s:  # open the file for writing user data
+        for key, value in users.items():
+            pickle.dump(value, out_s)  # write the object to the file
+
+    with open("./data/general.pickle", 'wb') as out_s:  # open the file for writing general data
+        pickle.dump(uptime, out_s)  # write the object to the file
+
+
 async def sendMessage(channel, msg):
     await theBot.send_message(channel, msg)
 
 
-def save():
-    with open("./data/users.pickle", 'wb') as out_s:  # open the file for writing
-        for key, value in users.items():
-            pickle.dump(value, out_s)  # write the object to the file
+async def update_view():
+    """Function to send a message update periodically."""
+    await theBot.wait_until_ready()
+    while True:  # set this up as an infinite loop
+        try:
+            if update_msg is not None:  # if we have set a place to update the display
+                msg = 'Uptime: ' + str(uptime) + "\n\n"
+                for key, user in users.items():
+                    msg = msg + str(key) + ":\n--Battery: " + str(
+                        user.getAttr("watts")) + "/" + str(
+                        user.getAttr("battery")) + "W\n--Overflow: " + str(
+                        user.getAttr("overflow")) + "W\n\n"
+                await theBot.edit_message(update_msg, msg)  # send the edit request
+        except Error:
+            print("probably a timeout error here")
+        await asyncio.sleep(update_period)  # wait a specified amount of time
 
 
 # These lines MUST be the end of the file. Any code after it might not run properly.
